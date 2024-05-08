79c1114a3e04cfa4b65cbc9e05f54ef5406b0c09
Dm-Edem <168981725+Dm-Edem@users.noreply.github.com>
2024-05-08 06:44:38 -0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,13 @@
+
+/*=======================typing effect===============*/
+
+const typed = new Typed('multiple-text', {
+    strings: ['Front-End Developer', 'Web Designer', 'Video Editor'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+
+
